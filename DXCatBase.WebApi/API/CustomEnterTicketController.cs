833f84751b91713e6efd16333d2ae3b032ea703c
andrey <andrey.kozhevnikov@devexpress.com>
2024-11-20 15:13:24 +0400

    format

--- DXCatBase.WebApi/API/CustomEnterTicketController.cs
+++ DXCatBase.WebApi/API/CustomEnterTicketController.cs
@@ -5,60 +5,60 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using static System.Net.Mime.MediaTypeNames;
 
-namespace DXCatBase.WebApi.API {
+namespace DXCatBase.WebApi.API;
 
-    [Route("api/[controller]")]
-    [ApiController]
-    
-    public class CustomEnterTicketController :ControllerBase {
-        private readonly IDataService dataService;
+[Route("api/[controller]")]
+[ApiController]
 
-        public CustomEnterTicketController(IDataService dataService) {
-            this.dataService = dataService;
+public class CustomEnterTicketController :ControllerBase {
+    private readonly IDataService dataService;
+
+    public CustomEnterTicketController(IDataService dataService) {
+        this.dataService = dataService;
+    }
+
+
+    [HttpPost]
+    public void Post([FromBody] TicketDataStub ticketStub) {
+        var objectSpace = dataService.GetObjectSpace(typeof(TicketData));
+
+        var existingTicketData = objectSpace.FindObject<TicketData>(CriteriaOperator.FromLambda<TicketData>(x => x.TicketId == ticketStub.TicketId));
+        if(existingTicketData != null) {
+            return;
         }
+        var ticket = objectSpace.CreateObject<TicketData>();
+        ticket.Subject = ticketStub.Subject;
+        ticket.TicketId = ticketStub.TicketId;
+        ticket.FeatureId = ticketStub.FeatureId;
+        ticket.Question = ticketStub.Question;
 
+        objectSpace.CommitChanges();
 
-        [HttpPost]
-        public void Post([FromBody] TicketDataStub ticketStub) {
-            var objectSpace = dataService.GetObjectSpace(typeof(TicketData));
+    }
 
-            var existingTicketData = objectSpace.FindObject<TicketData>(CriteriaOperator.FromLambda<TicketData>(x => x.TicketId == ticketStub.TicketId));
-            if(existingTicketData != null) {
-                return;
-            }
-            var ticket = objectSpace.CreateObject<TicketData>();
-            ticket.Subject = ticketStub.Subject;
-            ticket.TicketId = ticketStub.TicketId;
-            ticket.FeatureId = ticketStub.FeatureId;
-            ticket.Question = ticketStub.Question;
-
-            objectSpace.CommitChanges();
-
-        }
-
-        [HttpGet]
-        public string Get(string ticketStub) {
-
-       
+    [HttpGet]
+    public string Get(string ticketStub) {
 
 
 
-            return null;
-        }
 
-        [HttpGet("{id}")]
-        public string Get(int id) {
-            return "value";
-        }
 
-    
+        return null;
+    }
 
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value) {
-        }
+    [HttpGet("{id}")]
+    public string Get(int id) {
+        return "value";
+    }
 
-        [HttpDelete("{id}")]
-        public void Delete(int id) {
-        }
+
+
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value) {
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id) {
     }
 }
+
--- DXCatBase.WebApi/API/EvaluateNewTextController.cs
+++ DXCatBase.WebApi/API/EvaluateNewTextController.cs
@@ -1,35 +1,47 @@
 ﻿using DevExpress.ExpressApp.WebApi.Services;
+using DXCatBase.Module.BusinessObjects;
 using DXCatBase.Module.Controllers;
 using dxTestSolution.Module.BusinessObjects;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DXCatBase.WebApi.API {
-    [Route("api/[controller]")]
-    [ApiController]
+namespace DXCatBase.WebApi.API;
+[Route("api/[controller]")]
+[ApiController]
 
-    public class EvaluateNewTextController :ControllerBase {
-        private readonly IDataService dataService;
+public class EvaluateNewTextController :ControllerBase {
+    private readonly IDataService dataService;
 
-        public EvaluateNewTextController(IDataService dataService) {
-            this.dataService = dataService;
-        }
-        [HttpPost]
-        public string Post([FromBody] TicketDataStub ticketStub) {
-            var os = dataService.GetObjectSpace(typeof(TicketData));
+    public EvaluateNewTextController(IDataService dataService) {
+        this.dataService = dataService;
+    }
+    [HttpPost]
+    public string Post([FromBody] TicketDataStub ticketStub) {
+        var os = dataService.GetObjectSpace(typeof(TicketData));
 
-            var lst = os.GetObjects<FeaturePrompt>();
-            var intro = "You will get several tags. For each tag there is a promt that describes whether a text fits to the tag. Based on this info you need to determine in percentage how well each tag fits to the new text. Prepare answer as a JSON string in the format <tagName,tagId,percentage>. Don't wrap the result in '```json' - '```' strings";
-            var tags = lst.Select(x => new { tagName = x.Feature.Name, prompt = x.Prompt, tagId = x.Feature.Oid });
-            var tagsJson = Newtonsoft.Json.JsonConvert.SerializeObject(tags);
-            var newTicketText = Newtonsoft.Json.JsonConvert.SerializeObject(ticketStub);
+        var lst = os.GetObjects<FeaturePrompt>();
+        var intro = @"You will get several tags. For each tag there is a promt that describes whether a text fits to the tag. 
+            Based on this info you need to determine in percentage how well each tag fits to the new text. 
+            Prepare answer as a JSON string in the format <""tagName"":tagName,""tagId"":tagId,""percentage"":percentage>. 
+            Don't wrap the result in '```json' - '```' strings";
+        var tags = lst.Select(x => new { tagName = x.Feature.Name, prompt = x.Prompt, tagId = x.Feature.Oid });
+        var tagsJson = Newtonsoft.Json.JsonConvert.SerializeObject(tags);
+        var newTicketText = Newtonsoft.Json.JsonConvert.SerializeObject(ticketStub);
 
-            var client = GeneratePromptsController.GetClient();
-            var result = client.CompleteChat(intro, tagsJson, newTicketText);
+        var client = GeneratePromptsController.GetClient();
+        var result = client.CompleteChat(intro, tagsJson, newTicketText);
 
-            var resultText = result.Value.Content[0].Text;
+        var resultText = result.Value.Content[0].Text;
 
-            return resultText;
 
-        }
+
+
+        return resultText;
+
+    }
+
+
+    public List<TagAIResult> GetAIResults(string input) {
+        return null;
     }
 }
+
