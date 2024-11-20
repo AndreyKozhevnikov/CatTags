33c46f667db49c35c658a66f8414eaf509dff9b4
andrey <andrey.kozhevnikov@devexpress.com>
2024-11-20 15:16:13 +0400

    add test stub

---
+++ DXCatBase.Module/BusinessObjects/TagAIResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXCatBase.Module.BusinessObjects {
+    public class TagAIResult {
+        public string tagName { get; set; }
+        public string tagId { get; set; }
+        public int percentage { get; set; }
+    }
+}
---
+++ DXCatBase.WebApi/Tests/EvaluateNewTextControllerTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace DXCatBase.WebApi.Tests;
+[TestFixture]
+public class EvaluateNewTextControllerTests {
+    [Test]
+    public void EvaluateNewText() {
+        Assert.AreEqual(1, 2);
+    }
+}
